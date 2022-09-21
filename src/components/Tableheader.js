81c708046475f04dffd4f2f5cfd7a67f9f0bc7d1
aslam-16 <slamaslam466@gmail.com>
2022-09-21 20:11:27 +0530

    final-commit

--- src/components/Tableheader.js
+++ src/components/Tableheader.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Tableheader({type}) {
    let vehicleheader =  ['VEHICLE TYPE', 'VEHICLE NUMBER', 'DATE/TIME', 'TOLL NAME', 'TARIFF'];
-   let tollheader =  ['TOLL NAME', 'CARE/JEEP/VAN', 'LCV', 'TRUCK/BUS', 'HEAVY VEHICLE']
+   let tollheader =  ['TOLL NAME', 'CARE/JEEP/VAN', 'LCV', 'TRUCK/BUS', 'HEAVY VEHICLE','DELETE']
   return (
       <thead>
           <tr>
